a9b9b632eb627c0f97bcd0940859c49a167aa02b
Viktor <jude.peper@gmail.com>
2022-10-23 13:09:41 +0200

    add start files

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import { Notify } from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const { height: cardHeight } = document
+  .querySelector('.gallery')
+  .firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+  top: cardHeight * 2,
+  behavior: 'smooth',
+});
